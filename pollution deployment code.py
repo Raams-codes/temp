cc1ff2a9f6d7501df5a840b7d5b859ec152ce93d
Raams-codes <134188756+Raams-codes@users.noreply.github.com>
2023-08-02 20:25:02 +0530

    Update pollution deployment code.py

--- pollution deployment code.py
+++ pollution deployment code.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[6]:
 
 
-import numpy as np
-import pickle
-import pandas as pd
 import streamlit as st
 
-'''
-if (i=="delhi"):
-    pickle_in=open("delhi_VAR.pkl","rb")
-    delhi_VAR=pickle.load(pickle_in)
-elif(i=="patna"):
-    pickle_in=open("patna_VAR","rb")
-    patna_VAR=pickle.load(pickle_in)
-elif(i=="visakhapatnam"):
-    pickle_in=open("visakhapatnam_VAR","rb")
-    visakhapatnam_VAR=pickle.load(pickle_in)
-elif(i=="chandigarh"):
-    pickle_in=open("chandigarh_VAR","rb")
-    chandigarh_VAR=pickle.load(pickle_in)
-'''
-# In[ ]:
+
+# In[7]:
+
+
 st.write("Hello World")
 
 
@@ -34,8 +20,7 @@ st.text_input("Enter City Name")
 
 
 # In[ ]:
+
+
+if st.button("Submit"):
   write("Hi we are processing")
-
-if st.button("Predict"):
-
-# In[ ]:
