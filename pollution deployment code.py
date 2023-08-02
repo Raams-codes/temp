f38a4604ba28c3c07effe67526832d4560ccba2d
Raams-codes <134188756+Raams-codes@users.noreply.github.com>
2023-08-02 19:20:56 +0530

    Add files via upload

---
+++ pollution deployment code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st
+
+if (i=="delhi"):
+    pickle_in=open("delhi_VAR.pkl","rb")
+    delhi_VAR=pickle.load(pickle_in)
+elif(i=="patna"):
+    pickle_in=open("patna_VAR","rb")
+    patna_VAR=pickle.load(pickle_in)
+elif(i=="visakhapatnam"):
+    pickle_in=open("visakhapatnam_VAR","rb")
+    visakhapatnam_VAR=pickle.load(pickle_in)
+elif(i=="chandigarh"):
+    pickle_in=open("chandigarh_VAR","rb")
+    chandigarh_VAR=pickle.load(pickle_in)
+
+if st.button("Predict"):
+
